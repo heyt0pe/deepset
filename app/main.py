a88a4f6740b573c2a7b3f372769f0272f5f81f30
heyt0pe <akinthope@gmail.com>
2022-10-09 18:19:14 -0400

    update

--- app/main.py
+++ app/main.py
@@ -19,11 +19,11 @@ api = Api(app)
 
 
 lfqa_document_store = InMemoryDocumentStore(embedding_dim=128, return_embedding=True)
-rag_document_store = InMemoryDocumentStore(embedding_dim=128, return_embedding=True)
+#rag_document_store = InMemoryDocumentStore(embedding_dim=128, return_embedding=True)
 extractive_document_store = InMemoryDocumentStore(embedding_dim=128, return_embedding=True)
 
 lfqa_document_store.delete_documents()
-rag_document_store.delete_documents()
+#rag_document_store.delete_documents()
 extractive_document_store.delete_documents()
 
 docs = convert_files_to_docs(dir_path='app/docs')
@@ -38,43 +38,43 @@ preprocessor = PreProcessor(
 docs = preprocessor.process(docs)
 
 lfqa_document_store.write_documents(docs)
-rag_document_store.write_documents(docs)
+#rag_document_store.write_documents(docs)
 extractive_document_store.write_documents(docs)
 
 lfqa_retriever = DensePassageRetriever(
-    document_store=lfqa_document_store,
+   document_store=lfqa_document_store,
     query_embedding_model="vblagoje/dpr-question_encoder-single-lfqa-wiki",
-    passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
+   passage_embedding_model="vblagoje/dpr-ctx_encoder-single-lfqa-wiki",
 )
-rag_retriever = DensePassageRetriever(
-    document_store=rag_document_store,
-    query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
-    passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
-    use_gpu=True,
-    embed_title=True,
-)
+#rag_retriever = DensePassageRetriever(
+    #document_store=rag_document_store,
+    #query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+   # passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
+   # use_gpu=True,
+    #embed_title=True,
+#)
 extractive_retriever = TfidfRetriever(document_store=extractive_document_store)
 
 lfqa_document_store.update_embeddings(retriever=lfqa_retriever)
-rag_document_store.update_embeddings(retriever=lfqa_retriever)
+#rag_document_store.update_embeddings(retriever=lfqa_retriever)
 
 lfqa_generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa")
-rag_generator = RAGenerator(
-    model_name_or_path="facebook/rag-token-nq",
-    use_gpu=True,
-    top_k=1,
-    max_length=200,
-    min_length=2,
-    embed_title=True,
-    num_beams=2,
-)
+#rag_generator = RAGenerator(
+ #   model_name_or_path="facebook/rag-token-nq",
+  #  use_gpu=True,
+   # top_k=1,
+    #max_length=200,
+    #min_length=2,
+    #embed_title=True,
+    #num_beams=2,
+#)
 extractive_reader = FARMReader(
     model_name_or_path="deepset/roberta-base-squad2")
 
 lfqa_pipe = GenerativeQAPipeline(
     generator=lfqa_generator, retriever=lfqa_retriever)
-rag_pipe = GenerativeQAPipeline(
-    generator=rag_generator, retriever=rag_retriever)
+#rag_pipe = GenerativeQAPipeline(
+ #   generator=rag_generator, retriever=rag_retriever)
 extractive_pipe = ExtractiveQAPipeline(
     reader=extractive_reader, retriever=extractive_retriever)
 
@@ -125,7 +125,7 @@ class LFQAQuery(Resource):
         except Exception as e:
             return {"error": True, "msg": str(e)}
 
-
+'''
 class RAGQuery(Resource):
     def post(self):
         try:
@@ -154,7 +154,7 @@ class RAGQuery(Resource):
         except Exception as e:
             return {"error": True, "msg": str(e)}
 
-
+'''
 class ExtractiveQuery(Resource):
     def post(self):
         try:
@@ -185,5 +185,5 @@ class ExtractiveQuery(Resource):
 
 
 api.add_resource(LFQAQuery, '/lfqa-query')
-api.add_resource(RAGQuery, '/rag-query')
+#api.add_resource(RAGQuery, '/rag-query')
 api.add_resource(ExtractiveQuery, '/extractive-query')
